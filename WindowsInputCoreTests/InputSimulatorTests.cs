3e3bb84a96bffd653eea218728bc4b5bac4de95b
MichelMichels <michel@miche.ls>
2023-01-25 14:09:52 +0100

    Ported to .NET 7

---
+++ WindowsInputCoreTests/InputSimulatorTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsInputCore.Native;
+
+namespace WindowsInputCore.Tests
+{
+    [TestClass()]
+    public class InputSimulatorTests
+    {
+        [TestMethod]
+        public void OpenWindowsExplorer()
+        {
+            var sim = new InputSimulator();
+            sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.VK_E);
+        }
+
+        [TestMethod]
+        public void SayHello()
+        {
+            var sim = new InputSimulator();
+            sim.Keyboard
+               .ModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.VK_R)
+               .Sleep(1000)
+               .TextEntry("notepad")
+               .Sleep(1000)
+               .KeyPress(VirtualKeyCode.RETURN)
+               .Sleep(1000)
+               .TextEntry("These are your orders if you choose to accept them...")
+               .TextEntry("This message will self destruct in 5 seconds.")
+               .Sleep(5000)
+               .ModifiedKeyStroke(VirtualKeyCode.MENU, VirtualKeyCode.SPACE)
+               .KeyPress(VirtualKeyCode.DOWN)
+               .KeyPress(VirtualKeyCode.RETURN);
+
+            var i = 10;
+            while (i-- > 0)
+            {
+                sim.Keyboard.KeyPress(VirtualKeyCode.DOWN).Sleep(100);
+            }
+
+            sim.Keyboard
+               .KeyPress(VirtualKeyCode.RETURN)
+               .Sleep(1000)
+               .ModifiedKeyStroke(VirtualKeyCode.MENU, VirtualKeyCode.F4)
+               .KeyPress(VirtualKeyCode.VK_N);
+        }
+
+        [TestMethod]
+        public void AnotherTest()
+        {
+            var sim = new InputSimulator();
+            sim.Keyboard.KeyPress(VirtualKeyCode.SPACE);
+
+            sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.VK_R)
+               .Sleep(1000)
+               .TextEntry("mspaint")
+               .Sleep(1000)
+               .KeyPress(VirtualKeyCode.RETURN)
+               .Sleep(1000)
+               .Mouse
+               .LeftButtonDown()
+               .MoveMouseToPositionOnVirtualDesktop(65535 / 2, 65535 / 2)
+               .LeftButtonUp();
+
+        }
+
+        [TestMethod]
+        public void TestMouseMoveTo()
+        {
+            var sim = new InputSimulator();
+            sim.Mouse
+               .MoveMouseTo(0, 0)
+               .Sleep(1000)
+               .MoveMouseTo(65535, 65535)
+               .Sleep(1000)
+               .MoveMouseTo(65535 / 2, 65535 / 2);
+        }
+    }
+}
